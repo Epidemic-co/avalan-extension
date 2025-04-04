8e5a3ec7066f121e3d0e75da9bcee404fdbb8d09
matevz-ap <47795510+matevz-ap@users.noreply.github.com>
2025-04-04 14:35:25 +0200

    Fix auto closing posts and not clickable

--- content-script.js
+++ content-script.js
@@ -24,6 +24,7 @@ async function newPosts(posts) {
     getMentions(post);
     num_of_posts += 1;
     post.classList.add("avalan-post");
+    if (num_of_posts > 12) break;
     this.setTimeout(() => {post.focus({ preventScroll: true })}, 100);
   }
 }
